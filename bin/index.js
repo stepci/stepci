14c1d0713d24d3d7f64015257674aa7852eec006
Mish Ushakov <mishushakov@users.noreply.github.com>
2022-09-27 12:11:40 +0200

    Use new events

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@ import fs from 'fs'
 import exit from 'exit'
 import yaml from 'yaml'
 import chalk from 'chalk'
+import { EventEmitter } from 'node:events'
 
 const labels = {
   status: "Status",
@@ -17,58 +18,55 @@ const labels = {
   selector: "Selector"
 }
 
+const ee = new EventEmitter()
+ee.on('result', renderStep)
+ee.on('done', result => {
+  console.log('\n' + (result.passed ? chalk.green('✔ ') : chalk.red('✕ ')) + chalk.white(result.workflow.name) + ' (' + chalk.gray(result.workflow.path) + ') ' + (result.passed ? 'passed' :'failed') + ' in ' + result.duration / 1000 + 's')
+  if (!result.passed) exit(5)
+})
+
 // Render output
-function render (result, path) {
-  console.log((result.passed ? chalk.green('✔ ') : chalk.red('✕ ')) + chalk.white(result.workflow.name) + ' (' + chalk.gray(path) + ')' + ' in ' + result.duration / 1000 + 's')
+function renderStep (step) {
+  console.log('\n' + chalk.bgWhite.bold.black(` ${step.name} `) + (step.passed ? chalk.bgGreenBright.bold(' PASSED ') : chalk.bgRed.bold(' FAILED ')) + ' in ' + step.duration / 1000 + 's')
 
-  result.result.forEach((step, i) => {
-    console.log('\n' + chalk.bgWhite.bold.black(` ${step.name} `) + (step.passed ? chalk.bgGreenBright.bold(' PASSED ') : chalk.bgRed.bold(' FAILED ')) + ' in ' + step.duration / 1000 + 's')
+  if (step.failed || step.skipped) {
+    return console.log('\n' + chalk.yellow('⚠︎ ') + (step.failReason || 'Step was skipped') + '\n')
+  }
 
-    if (step.failed || step.skipped) {
-      return console.log('\n' + chalk.yellow('⚠︎ ') + (step.failReason || 'Step was skipped') + '\n')
-    }
+  console.log(chalk.bold('\nRequest\n'))
+  console.log(chalk.bgWhite.bold(` ${step.request.method} `) + ' ' + step.request.url + ' ' + chalk.bgGray.bold(` ${step.response.status} ${step.response.statusText} `))
+  console.log(chalk.bold('\nChecks'))
 
-    console.log(chalk.bold('\nRequest\n'))
-    console.log(chalk.bgWhite.bold(` ${step.request.method} `) + ' ' + step.request.url + ' ' + chalk.bgGray.bold(` ${step.response.status} ${step.response.statusText} `))
-    console.log(chalk.bold('\nChecks'))
-
-    for (const check in step.checks) {
-      console.log('\n' + labels[check])
-      if (['jsonpath', 'xpath', 'headers', 'selector'].includes(check)) {
-        for (const component in step.checks[check]) {
-          step.checks[check][component].passed
-            ? console.log(chalk.green('✔ ') + chalk.bold(component) + ': ' + step.checks[check][component].given)
-            : console.log(chalk.red('✕ ') + chalk.bold(component) + ': ' + step.checks[check][component].given + ' (expected ' + step.checks[check][component].expected + ')')
-        }
-      } else {
-        step.checks[check].passed
-          ? console.log(chalk.green('✔ ') + chalk.bold(step.checks[check].given))
-          : console.log(chalk.red('✕ ') + chalk.bold(step.checks[check].given) + ' (expected ' + step.checks[check].expected + ')')
+  for (const check in step.checks) {
+    console.log('\n' + labels[check])
+    if (['jsonpath', 'xpath', 'headers', 'selector'].includes(check)) {
+      for (const component in step.checks[check]) {
+        step.checks[check][component].passed
+          ? console.log(chalk.green('✔ ') + chalk.bold(component) + ': ' + step.checks[check][component].given)
+          : console.log(chalk.red('✕ ') + chalk.bold(component) + ': ' + step.checks[check][component].given + ' (expected ' + step.checks[check][component].expected + ')')
       }
+    } else {
+      step.checks[check].passed
+        ? console.log(chalk.green('✔ ') + chalk.bold(step.checks[check].given))
+        : console.log(chalk.red('✕ ') + chalk.bold(step.checks[check].given) + ' (expected ' + step.checks[check].expected + ')')
     }
-  })
+  }
 }
 
 // Run workflow files
-async function runWorkflows (path) {
+function runWorkflow (path) {
+  const workflowFile = fs.readFileSync(path).toString()
+  const config = yaml.parse(workflowFile)
+  run({ ...config, path }, { ee })
+}
+
+// Load workflow files
+function loadWorkflows (path) {
   if (fs.lstatSync(path).isDirectory()) {
     const files = fs.readdirSync(path).filter(file => file.includes('yml') || file.includes('yaml'))
-    files.forEach(async (file, i) => {
-      const workflowFile = fs.readFileSync(path + file).toString()
-      const config = yaml.parse(workflowFile)
-      const result = await run(config)
-
-      render(result, path + file)
-      if (i !== files.length - 1) console.log('-'.repeat(process.stdout.columns))
-      if (!result.passed) exit(5)
-    })
+    files.forEach(file => runWorkflow(path + file))
   } else {
-    const workflowFile = fs.readFileSync(path).toString()
-    const config = yaml.parse(workflowFile)
-    const result = await run(config)
-
-    render(result, path)
-    if (!result.passed) exit(5)
+    runWorkflow(path)
   }
 }
 
@@ -78,5 +76,5 @@ yargs(hideBin(process.argv))
       .positional('workflow', {
         describe: 'workflow path (file or a directory)'
       })
-  }, (argv) => runWorkflows(argv.workflow))
+  }, (argv) => loadWorkflows(argv.workflow))
   .parse()

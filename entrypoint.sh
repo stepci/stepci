046a3692c1722e59a2d40be58c2d80589ecec134
yu-fukusako <yu-fukusako@kddi.com>
2023-03-27 16:29:49 +0900

    Fix to use some options in githubactions

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set +x
+
+EXECUTION_COMMAND="stepci run $1"
+
+if [ -n "$2" ]; then
+    EXECUTION_COMMAND="$EXECUTION_COMMAND -e $2"
+fi
+
+if [ -n "$3" ]; then
+    EXECUTION_COMMAND="$EXECUTION_COMMAND -s $3"
+fi
+
+if [ "$4" == "true" ]; then
+    EXECUTION_COMMAND="$EXECUTION_COMMAND --hide"
+fi
+
+eval $EXECUTION_COMMAND
